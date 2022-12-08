07ef65f7df41596889c298c63946481dc02d36cf
erickssguerra <erickssguerra@gmail.com>
2022-12-08 11:53:07 -0300

    feat: import router into server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express, { json } from "express";
-import connection from "./database/database.js";
+import router from "./routes/router.js";
 import cors from "cors";
 
 const server = express();
 
 server.use(cors());
 server.use(json());
+server.use(router);
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => {
