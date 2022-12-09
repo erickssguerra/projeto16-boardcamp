79cfce977fb9b09b3c5b333cd8db0d82c0b81323
erickssguerra <erickssguerra@gmail.com>
2022-12-09 11:57:57 -0300

    fix: add semi-colon in queries

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -4,8 +4,8 @@ import {
   getCategory,
 } from "../controllers/categories.controllers.js";
 
-import { categorySchemaValidation } from "../middleware/categorySchemaValidation.middleware.js";
-import { existingCategory } from "../middleware/categoryExistingValidation.middleware.js";
+import { categorySchemaValidation } from "../middlewares/categorySchemaValidation.middleware.js";
+import { existingCategory } from "../middlewares/categoryExistingValidation.middleware.js";
 
 const categoriesRouter = Router();
 
