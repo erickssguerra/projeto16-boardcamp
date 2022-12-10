b422f82547649e6e65d6143fbcf7dc46cc2f99fa
erickssguerra <erickssguerra@gmail.com>
2022-12-09 21:49:59 -0300

    feat: post customer (finalized)

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -2,5 +2,21 @@ import connectionDB from "../database/database.js";
 
 export async function postCustomer(req, res) {
   const costumerObject = res.locals.validatedCostumer;
-  res.status(201).send(costumerObject);
+  const { name, phone, cpf, birthday } = costumerObject;
+  try {
+    await connectionDB.query(
+      `
+    INSERT INTO customers 
+      (name, phone, cpf, birthday) 
+    VALUES
+      ($1, $2, $3, $4)
+    ;`,
+      [name, phone, cpf, birthday]
+    );
+    console.log("controller: postGame passed!");
+    return res.status(201).send({ message: "Usuário cadastrado com sucesso!" });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
