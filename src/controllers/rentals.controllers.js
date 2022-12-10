7911c71c02294297868f3828e832e41f93f6b7a1
erickssguerra <erickssguerra@gmail.com>
2022-12-10 18:53:54 -0300

    feat: post rentals (incomplete)

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -1,10 +1,41 @@
 import chalk from "chalk";
 import connectionDB from "../database/database.js";
+import dayjs from "dayjs";
 
 export async function getRentals(req, res) {}
 
 export async function postRental(req, res) {
   const rentalObject = res.locals.rentalExistingGame;
-  console.log(chalk.green("controller: postRental concluded!"));
-  res.status(201).send(rentalObject);
+  const pricePerDay = res.locals.pricePerDayGame;
+  const { customerId, gameId, daysRented } = rentalObject;
+  const rentDate = dayjs().format("YYYY-MM-DD");
+  const returnDate = null;
+  const delayFee = null;
+  const originalPrice = pricePerDay * daysRented;
+  try {
+    await connectionDB.query(
+      `
+    INSERT INTO rentals
+        ("customerId", "gameId", "rentDate",
+        "daysRented", "returnDate", "originalPrice",
+        "delayFee")
+    VALUES
+        ($1, $2, $3, $4, $5, $6, $7)
+    ;`,
+      [
+        customerId,
+        gameId,
+        rentDate,
+        daysRented,
+        returnDate,
+        originalPrice,
+        delayFee,
+      ]
+    );
+    console.log(chalk.green("controller: postRental concluded!"));
+    res.status(201).send({ message: "Aluguel cadastrado com sucesso!" });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 }
