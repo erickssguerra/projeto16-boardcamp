f08ea9fff0324b4bca8cf793ec34f96eb570df85
erickssguerra <erickssguerra@gmail.com>
2022-12-10 18:53:27 -0300

    refact: typo file name (rentalExistingCustomer)

--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { postRental } from "../controllers/rentals.controllers.js";
 import { rentalSchemaValidation } from "../middlewares/rentals/rentalSchemaValidation.middleware.js";
-import { existingCustomer } from "../middlewares/rentals/rentalsExistingCustumer.middleware.js";
+import { existingCustomer } from "../middlewares/rentals/rentalExistingCustomer.middleware.js";
 import { existingGame } from "../middlewares/rentals/rentalExistingGame.middleware.js";
 
 const rentalsRouter = Router();
