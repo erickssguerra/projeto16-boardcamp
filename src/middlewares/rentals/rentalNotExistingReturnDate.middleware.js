62921bdcf122a823cb4f6ff2d90394193f396b1b
erickssguerra <erickssguerra@gmail.com>
2022-12-11 00:53:29 -0300

    feat: post returnRental/:id (MW: notExistingReturnDate)

--- src/middlewares/rentals/rentalNotExistingReturnDate.middleware.js
+++ src/middlewares/rentals/rentalNotExistingReturnDate.middleware.js
@@ -3,7 +3,23 @@ import chalk from "chalk";
 
 export async function notExistingReturnDate(req, res, next) {
   const id = res.locals.existingRentalId;
-  console.log(chalk.yellow("middleware: notExistingReturnDate passed..."));
-  res.locals.notExistingReturnDate = id;
+  try {
+    const rentalObject = await connectionDB.query(
+      `SELECT * FROM rentals WHERE id = $1
+    ;`,
+      [id]
+    );
+    const returnDate = rentalObject.rows[0].returnDate;
+    if (returnDate) {
+      console.log(chalk.red("middleware: notExistingReturnDate blocked!"));
+      return res.status(400).send({ message: "Aluguel já foi finalizado!" });
+    } else {
+      console.log(chalk.yellow("middleware: notExistingReturnDate passed..."));
+      res.locals.notExistingReturnDate = id;
+    }
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
   next();
 }
