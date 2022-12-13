51199571f0bd6059181015bc4889ad483b24bd4d
erickssguerra <erickssguerra@gmail.com>
2022-12-12 23:20:13 -0300

    v2.1 feat: queries 'offset' and 'limit' @ get games

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -3,49 +3,66 @@ import connectionDB from "../database/database.js";
 
 export async function getGames(req, res) {
   const nameQuery = req.query.name?.trim();
+  const offset = Number(req.query.offset);
+  const limit = Number(req.query.limit);
+  const getQuery = `
+      SELECT 
+        games.*, categories.name AS "categoryName"
+      FROM games 
+      JOIN categories
+      ON games."categoryId" = categories.id
+      `;
+  const getNameQuery = `
+      WHERE 
+        (games.name) 
+      ILIKE CONCAT
+      (CAST($1 AS TEXT),'%')
+      `;
   try {
-    if (nameQuery) {
-      const games = await connectionDB.query(
-        `
-        SELECT 
-          games.*, categories.name AS "categoryName"
-        FROM 
-          games 
-        JOIN
-          categories
-        ON
-          games."categoryId" = categories.id
-        WHERE
-          (games.name) 
-        ILIKE CONCAT
-          (CAST($1 AS TEXT),'%')
-        ;`,
-        [nameQuery]
-      );
-      if (games.rowCount) {
-        console.log(chalk.green("controller: getGames with query concluded!"));
-      } else {
-        console.log(
-          chalk.yellow("controller: getGames with query returned empty!")
-        );
-      }
-      return res.status(200).send(games.rows);
-    } else {
-      const games = await connectionDB.query(
-        `
-        SELECT 
-          games.*, categories.name AS "categoryName" 
-        FROM 
-          games
-        JOIN
-          categories
-        ON
-          games."categoryId" = categories.id
-        ;`
-      );
-      console.log(chalk.green("controller: getGames concluded!"));
-      return res.status(200).send(games.rows);
+    if (!nameQuery && !offset && !limit) {
+      const games = await connectionDB.query(`${getQuery};`);
+      console.log(chalk.green("controller: getGames with no queries concluded!"));
+      res.status(200).send(games.rows);
+    } else if (nameQuery && !offset && !limit) {
+      const games = await connectionDB.query(`${getQuery} ${getNameQuery};`, [nameQuery]);
+      console.log(chalk.green("controller: getGames with query 'name' concluded!"));
+      res.status(200).send(games.rows);
+
+    } else if (nameQuery && offset && !limit) {
+      const games = await connectionDB.query(`${getQuery} ${getNameQuery} OFFSET $2;`,
+        [nameQuery, offset]);
+      console.log(chalk.green("controller: getGames with queries 'name' and 'offset' concluded!"));
+      res.status(200).send(games.rows);
+
+    } else if (nameQuery && offset && limit) {
+      const games = await connectionDB.query(`${getQuery} ${getNameQuery} OFFSET $2 LIMIT $3;`,
+        [nameQuery, offset, limit]);
+      console.log(chalk.green("controller: getGames with queries 'name', 'offset' and 'limit' concluded!"));
+      res.status(200).send(games.rows);
+
+    } else if (nameQuery && !offset && limit) {
+      const games = await connectionDB.query(`${getQuery} ${getNameQuery} LIMIT $2;`,
+      [nameQuery, limit]);
+      console.log(chalk.green("controller: getGames with queries 'name' and 'limit' concluded!"));
+      res.status(200).send(games.rows);
+
+    } else if (!nameQuery && offset && limit) {
+      const games = await connectionDB.query(`${getQuery} OFFSET $1 LIMIT $2;`,
+        [offset, limit]);
+      console.log(chalk.green("controller: getGames with queries 'offset' and 'limit' concluded!"));
+      res.status(200).send(games.rows);
+
+    } else if (!nameQuery && !offset && limit) {
+      const games = await connectionDB.query(`${getQuery} LIMIT $1;`, [limit]);
+      console.log(chalk.green("controller: getGames with query 'limit' concluded!"));
+      res.status(200).send(games.rows);
+
+    } else if (!nameQuery && offset && !limit) {
+      const games = await connectionDB.query(`${getQuery} OFFSET $1;`, [offset]);
+      console.log(chalk.green("controller: getGames with query 'offset' concluded!"));
+      res.status(200).send(games.rows);
     }
+    
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
